a87537501efe6efe941fcbc6a6f9c84b4c51b835
U-DerRM-PC\DerRM <DerRM@DerRM-PC.(none)>
2012-04-26 13:36:56 +0200

    adding prototypes

---
+++ GamePrototypes/Fall Ball/Fall Ball/Fall Ball/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über den folgenden 
+// Satz von Attributen kontrolliert. Ändern Sie diese Attributwerte, um die mit einer Assembly
+// verbundenen Informationen zu ändern.
+[assembly: AssemblyTitle("Fall Ball")]
+[assembly: AssemblyProduct("Fall Ball")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Bei Einstellung von ComVisible auf falsch sind die Typen in dieser Assembly für 
+// COM-Komponenten nicht sichtbar.  Wenn Sie von COM aus auf einen Typ in dieser Assembly 
+// zugreifen müssen, stellen Sie das Attribut ComVisible bei diesem Typ auf wahr ein. Nur Windows-
+// Assemblys unterstützen COM.
+[assembly: ComVisible(false)]
+
+// Auf Windows gilt die folgende GUID für die ID von typelib, wenn dieses
+// COM ausgesetzt ist. Auf anderen Plattformen identifiziert sie den
+// Titelspeichercontainer bei Bereitstellung dieser Assembly auf dem Gerät eindeutig.
+[assembly: Guid("bf9b64be-3be9-46cd-af18-532b58ef759d")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Größere Version
+//      Kleinere Version 
+//      Build-Nummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
