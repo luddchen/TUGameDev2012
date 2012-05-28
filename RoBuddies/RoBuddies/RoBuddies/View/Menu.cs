077a6c1347ddcdc90e56b6ce29a4f28f1aba9956
Ludwig Ringler <ludwig.ringler@mailbox.tu-berlin.de>
2012-05-28 19:04:34 +0200

    some menu stuff

--- RoBuddies/RoBuddies/RoBuddies/View/Menu.cs
+++ RoBuddies/RoBuddies/RoBuddies/View/Menu.cs
@@ -9,12 +9,15 @@ namespace RoBuddies.View
 {
     class Menu
     {
+        private float animationValue = 0.0f;
         private Texture2D square;
         private Rectangle squareDest;
         private Viewport viewport;
         private bool isVisible;
-        private Color squareColor = new Color(128,128,128,192);
+        private Color squareColor = Color.Black;
         private KeyboardState oldKeybordState;
+
+        private List<IHUDElement> AllMenuDecoration;
 
         /// <summary>
         /// prefered width of menu window
@@ -82,6 +85,17 @@ namespace RoBuddies.View
             this.IsVisible = false;
             this.oldKeybordState = Keyboard.GetState();
 
+
+            // some decoration -------------------------------------------------------------------
+            this.AllMenuDecoration = new List<IHUDElement>();
+            this.AllMenuDecoration.Add(new HUDString( "Robuddies Menu", this.Game.Content));
+            this.AllMenuDecoration.Add(new HUDTexture(this.Game.Content));
+            this.AllMenuDecoration.Add(new HUDTexture(this.Game.Content));
+            this.AllMenuDecoration.Add(new HUDTexture(this.Game.Content));
+            this.AllMenuDecoration.Add(new HUDTexture(this.Game.Content));
+            // end decoration -------------------------------------------------------------------
+
+
             this.DefaultPage = new MenuPage(this.Game.Content);
             this.ActivePage = this.DefaultPage;
         }
@@ -105,6 +119,19 @@ namespace RoBuddies.View
 
                 this.ActivePage.Viewport = this.Viewport;
             }
+
+            this.AllMenuDecoration[0].Position = new Vector2(this.Viewport.Width / 2 + (float)(Math.Cos(this.animationValue) * 1), 60 + (float)(Math.Sin(this.animationValue * 2) * 0.5f));
+            this.AllMenuDecoration[1].Position = new Vector2(42, 62);
+            this.AllMenuDecoration[2].Position = new Vector2(this.Viewport.Width - 42, 42);
+            this.AllMenuDecoration[3].Position = new Vector2(52, this.Viewport.Height - 42);
+            this.AllMenuDecoration[4].Position = new Vector2(this.Viewport.Width - 72, this.Viewport.Height - 62);
+            foreach (IHUDElement element in this.AllMenuDecoration)
+            {
+                element.Update(gameTime);
+            }
+
+            this.animationValue += 0.25f;
+            if (this.animationValue > MathHelper.Pi * 2) { this.animationValue = 0.0f; }
         }
 
         public void Draw(GameTime gameTime)
@@ -115,6 +142,11 @@ namespace RoBuddies.View
                 this.Game.SpriteBatch.Begin();
 
                 this.Game.SpriteBatch.Draw(this.square, this.squareDest, this.squareColor);
+
+                foreach (IHUDElement element in this.AllMenuDecoration)
+                {
+                    element.Draw(this.Game.SpriteBatch);
+                }
 
                 if (this.ActivePage != null)
                 {
--- RoBuddies/RoBuddies/RoBuddies/View/MenuPage.cs
+++ RoBuddies/RoBuddies/RoBuddies/View/MenuPage.cs
@@ -16,16 +16,10 @@ namespace RoBuddies.View
         public MenuPage(ContentManager content)
         {
             this.AllElements = new List<IHUDElement>();
-            this.AllElements.Add(new HUDString(content));
-            this.AllElements.Add(new HUDTexture(content));
-            this.AllElements.Add(new HUDTexture(content));
         }
 
         public void Update(GameTime gameTime)
         {
-            this.AllElements[0].Position = new Vector2(this.Viewport.Width/2, 20);
-            this.AllElements[1].Position = new Vector2(20, 20);
-            this.AllElements[2].Position = new Vector2(this.Viewport.Width - 20, 20);
             foreach (IHUDElement element in this.AllElements)
             {
                 element.Update(gameTime);
