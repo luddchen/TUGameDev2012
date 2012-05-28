0adbe73505128ced1a794e309382c12afed78379
Ludwig Ringler <ludwig.ringler@mailbox.tu-berlin.de>
2012-05-28 13:00:39 +0200

    move HUDElement to View and some implementation of Camera

--- RoBuddies/RoBuddies/RoBuddies/View/Camera.cs
+++ RoBuddies/RoBuddies/RoBuddies/View/Camera.cs
@@ -1,11 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+using Microsoft.Xna.Framework;
 
 namespace RoBuddies.View
 {
+
+    /// <summary>
+    /// representation of a global camera
+    /// </summary>
     class Camera
     {
+
+        /// <summary>
+        /// global position
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// global zoom
+        /// </summary>
+        public float Zoom { get; set; }
+
+        /// <summary>
+        /// global rotation
+        /// </summary>
+        public float Rotation { get; set; }
+
+
+        /// <summary>
+        /// creates a new camera object
+        /// </summary>
+        public Camera()
+        {
+            this.Position = Vector2.Zero;
+            this.Zoom = 1.0f;
+            this.Rotation = 0.0f;
+        }
+
+
+        /// <summary>
+        /// move the camera to given global coordinates
+        /// </summary>
+        /// <param name="to">target coordinates</param>
+        public void Move(Vector2 to)
+        {
+        }
+
+        /// <summary>
+        /// gets the View Matrix according given parallax
+        /// </summary>
+        /// <param name="parallax">parallax value</param>
+        /// <returns>view matrix</returns>
+        public Matrix GetViewMatrix(Vector2 parallax)
+        {
+            return Matrix.CreateScale(1, 1, 1);
+        }
     }
 }
