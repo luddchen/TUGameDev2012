cfa5d9d9605786b01f7a3517dda62e745236dcd8
x-wang <nicole.xiwang@gmail.com>
2012-05-28 13:08:22 -0700

    small changes in Robot.cs.

--- RoBuddies/RoBuddies/RoBuddies/Model/Robot.cs
+++ RoBuddies/RoBuddies/RoBuddies/Model/Robot.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
 namespace RoBuddies.Model
 {
 
@@ -9,5 +12,55 @@ namespace RoBuddies.Model
     /// </summary>
     class Robot
     {
+        private IBody lowerPart;
+        private IBody upperPart;
+        private IBody partsCombined;
+        private IBody head;
+        private IBody activePart;
+
+        private Level level;
+
+        public Robot(ContentManager content, Vector2 pos, Level level) 
+        {
+            this.level = level;
+            initRobot(content, pos);
+
+        }
+
+        private void initRobot(ContentManager content, Vector2 pos) 
+        { 
+        }
+
+        public IBody LowerPart 
+        {
+            get { return lowerPart; }
+        }
+
+        public IBody UpperPart 
+        {
+            get { return upperPart; }
+        }
+
+        public IBody PartsCombined
+        {
+            get { return partsCombined; }
+        }
+
+        public IBody Head 
+        {
+            get { return head; }
+            set { head = value; }
+        }
+
+        public IBody ActivePart
+        {
+            get { return activePart; }
+            set { activePart = value; }
+        }
+
+        public void Update() 
+        {
+        }
+
     }
 }
