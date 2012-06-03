c45ecfee2a6444ea3c7864e29379727981250f67
Christopher Sierigk <Chris.Sierigk@t-online.de>
2012-06-03 19:52:34 +0200

    walking animation added

--- RoBuddies/RoBuddies/RoBuddies/Control/StateMachines/PartsCombinedStateMachine.cs
+++ RoBuddies/RoBuddies/RoBuddies/Control/StateMachines/PartsCombinedStateMachine.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using FarseerPhysics.Dynamics;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using RoBuddies.Control.RobotStates;
 using RoBuddies.Control.RobotStates.Interfaces;
 using RoBuddies.Model;
@@ -15,13 +16,27 @@ namespace RoBuddies.Control.StateMachines
         public const String JUMP_STATE = "JumpState";
         public const String WALK_STATE = "WalkingState";
 
+        private const int START_WALKING = 5;
+        private const int STOP_WALKING = 24;
+        private const int END_ANIMATION = 80;
+
         private KeyboardState oldState;
         private Game game;
+        private List<Texture2D> textureList;
+        private float currentTextureIndex;
 
         public PartsCombinedStateMachine(IBody body, Game game)
             : base(body)
         {
             this.game = game;
+            this.textureList = new List<Texture2D>();
+
+            for (int i = 1; i <= END_ANIMATION; i++)
+            {
+                textureList.Add(game.Content.Load<Texture2D>("Sprites\\Robot\\BudBudi\\" + String.Format("{0:0000}", i)));
+            }
+
+            body.Texture = textureList[0];
         }
 
         public override void Update(GameTime gameTime)
@@ -38,6 +53,7 @@ namespace RoBuddies.Control.StateMachines
                 SwitchToState(WALK_STATE);
                 (Body as Body).LinearVelocity = new Vector2(-2, (Body as Body).LinearVelocity.Y);
                 Body.Effect = SpriteEffects.FlipHorizontally;
+                UpdateWalkAnimation(gameTime);
             }
 
             if (newState.IsKeyDown(Keys.Right))
@@ -45,9 +61,26 @@ namespace RoBuddies.Control.StateMachines
                 SwitchToState(WALK_STATE);
                 (Body as Body).LinearVelocity = new Vector2(2, (Body as Body).LinearVelocity.Y);
                 Body.Effect = SpriteEffects.None;
+                UpdateWalkAnimation(gameTime);
             }
 
             oldState = newState;
+        }
+
+        private void UpdateWalkAnimation(GameTime gameTime)
+        {
+            if (currentTextureIndex < START_WALKING)
+            {
+                currentTextureIndex = START_WALKING;
+            }
+
+            if (currentTextureIndex > STOP_WALKING)
+            {
+                currentTextureIndex = START_WALKING;
+            }
+
+            Body.Texture = textureList[(int) currentTextureIndex];
+            currentTextureIndex += 0.6f;
         }
 
         #region IPartsCombinedTransition Member
